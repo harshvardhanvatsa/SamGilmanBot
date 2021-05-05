68c0f13d1289e2c68e9737ca3442931b0a0d6125
harshvardhanvatsa <76729417+harshvardhanvatsa@users.noreply.github.com>
2021-05-05 17:22:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from keep_alive import keep_alive
+import discord
+import os
+
+client = discord.Client()
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('Hello Sam'):
+    await message.channel.send('Hello!')
+  
+  if message.content.startswith('How are you?'):
+    await message.channel.send('I am fine wbu?')  
+keep_alive()    
+client.run(os.getenv('TOKEN'))
